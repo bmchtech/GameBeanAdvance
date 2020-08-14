c9cb58beeb7e962083a5905f3149d728d537ae66
ZaydQumsieh <zayd@qumsieh.net>
2020-08-14 13:27:42 -0700

    added THUMB headers to jumptable
    Former-commit-id: 35a0c86225b94687ccfffc6a9402befb4505310f

--- src/jumptable/make-jumptable.py
+++ src/jumptable/make-jumptable.py
@@ -7,23 +7,24 @@ INPUT_FILE_NAME         = "test-jumptable.cpp"
 OUTPUT_HEADER_FILE      = "jumptable.h"
 OUTPUT_CPP_FILE         = "jumptable.cpp"
 FUNCTION_HEADER         = "void run_" 
-INSTRUCTION_SIZE        = 8
-JUMPTABLE_BIT_WIDTH     = 6
+INSTRUCTION_SIZE        = 16
+JUMPTABLE_BIT_WIDTH     = 8
 JUMPTABLE_EXCLUDED_BITS = INSTRUCTION_SIZE - JUMPTABLE_BIT_WIDTH
 CONDITIONAL_INCLUSION   = "@IF("
 JUMPTABLE_FORMAT_WIDTH  = 4
-
+EXCLUSION_HEADER        = "@EXCLUDE("
+DEFAULT_HEADER          = "@DEFAULT("
 
 # formatting for the output files
 HEADER_FILE_HEADER      = '''
 #ifndef JUMPTABLE_H
-#define JUMPTABLE_H\n\n'''
+#define JUMPTABLE_H\n\n'''[1:] # the [1:] is used to remove the beginning \n
 
 HEADER_FILE_FOOTER      = '''
 #endif'''
 
 CPP_FILE_HEADER         = '''
-#include "jumptable.h"\n\n'''
+#include "jumptable.h"\n\n'''[1:] # the [1:] is used to remove the beginning \n
 
 CPP_FILE_FOOTER         = ''''''
 
@@ -36,8 +37,14 @@ lines = open(INPUT_FILE_NAME, "r").read().split("\n")
 
 i = 0
 vars = {} # contain aliases for the bits in the opcode.
+exclusions = []
 num_vars = 0
 jumptable = [None] * pow(2, JUMPTABLE_BIT_WIDTH) 
+
+default_function = []
+
+
+
 
 def get_nth_bit(val, n):
     return (val >> n) & 1
@@ -45,7 +52,51 @@ def get_nth_bit(val, n):
 def set_nth_bit(val, n):
     return val | (1 << n)
 
+# get the possible exclusions given the tag, taking - into account
+def get_exclusions(tag):
+    dashes = []
+    exclusions = []
+    base = 0
+
+    for i in range(0, JUMPTABLE_BIT_WIDTH):
+        bit = tag[i]
+
+        if bit == '1':
+            base = set_nth_bit(base, JUMPTABLE_BIT_WIDTH - i - 1)
+        if bit == '-':
+            dashes.append(JUMPTABLE_BIT_WIDTH - i - 1)
+
+    for i in range(0, pow(2, len(dashes))):
+        new_base = base
+
+        for j in range(0, len(dashes)):
+            if get_nth_bit(i, j) == 1:
+                new_base = set_nth_bit(new_base, dashes[j])
+        exclusions.append(new_base)
+
+    return exclusions
+
+
+
+
 for i in range(0, len(lines)):
+    if lines[i].startswith(EXCLUSION_HEADER):
+        # we should exclude the following tag from the next function
+        tag = lines[i].split(EXCLUSION_HEADER)[1].split(")")[0]
+        exclusions += get_exclusions(tag)
+
+    # is this a default function
+    if lines[i].startswith(DEFAULT_HEADER):
+        if len(default_function) != 0:
+            print("Two default functions detected.")
+            print("Terminating.")
+            exit(1)
+
+        j = i + 2 # go to the start of the default function
+        while not lines[j].startswith("}"):
+            default_function.append(lines[j])
+            j += 1
+
     # is this a function definition
     if lines[i].startswith(FUNCTION_HEADER):
         # if so, let's parse through the function name and collect the vars
@@ -68,8 +119,8 @@ for i in range(0, len(lines)):
             current_bit -= 1
         
         # calculate the base given the number of excluded bits in the jumptable
-        base = int(base) << JUMPTABLE_EXCLUDED_BITS
-        
+        base = int(base, 2)
+
         # now, we know the next line is the function body itself
         # lets collect the function body first
         function = []
@@ -81,6 +132,16 @@ for i in range(0, len(lines)):
         # we gotta iterate through the lines possible bits...
         keys = list(vars.keys())
         for current_iteration in range(0, pow(2, num_vars)):
+            # calculate the index of the new function so we can check it against the exclusions list
+            new_base = base
+            for var in keys:
+                if get_nth_bit(current_iteration, keys.index(var)):
+                    new_base = set_nth_bit(new_base, vars[var] - JUMPTABLE_EXCLUDED_BITS)
+
+            # do we exclude this iteration?
+            if new_base in exclusions:
+                continue
+
             # current_iteration interpretted in binary
             # see if the line is a conditional inclusion
             new_function = []
@@ -96,13 +157,16 @@ for i in range(0, len(lines)):
                 # no? okay, lets add the line
                 new_function.append(line)
             
-            # we have the new function body. now we just gotta insert it into its rightful place.
-            new_base = base
-            for var in keys:
-                if get_nth_bit(current_iteration, keys.index(var)):
-                    new_base = set_nth_bit(new_base, vars[var] - JUMPTABLE_EXCLUDED_BITS)
+            # and now we insert the function into the jumptable
+            if (jumptable[new_base] != None):
+                print("Collision detected at: " + format(new_base, '#0' + str(JUMPTABLE_BIT_WIDTH + 2) + 'b')[2:])
+                print("Terminating.")
+                exit(1)
 
             jumptable[new_base] = new_function
+
+        # reset the exclusions
+        exclusions = []
 
 
 
@@ -126,6 +190,12 @@ for i in range(0, pow(2, JUMPTABLE_BIT_WIDTH)):
         cpp_file.write(function_name + " {\n")
         cpp_file.write('\n'.join(result_function))
         cpp_file.write("\n}\n\n")
+    else:
+        function_name = FUNCTION_HEADER + format(i, '#0' + str(JUMPTABLE_BIT_WIDTH + 2) + 'b')[2:] + "()"
+        header_file.write(function_name + ";\n")
+        cpp_file.write(function_name + " {\n")
+        cpp_file.write('\n'.join(default_function))
+        cpp_file.write("\n}\n\n")
 
 # and now we must loop again to put the actual jumptable in the cpp file
 function_names = list("run_" + format(i, '#0' + str(JUMPTABLE_BIT_WIDTH + 2) + 'b')[2:] for i in range(0, pow(2, JUMPTABLE_BIT_WIDTH)))
@@ -143,3 +213,5 @@ header_file.write(HEADER_FILE_FOOTER)
 cpp_file.write(CPP_FILE_FOOTER)
 
 # and fin
+print("Success!")
+exit(0)
--- src/jumptable/test-jumptable.cpp
+++ src/jumptable/test-jumptable.cpp
@@ -1,8 +1,120 @@
+// NOTE: this code is not meant to be run in its raw form. it should first be parsed by make-jumptable.py
+// with these settings:
+//     INSTRUCTION_SIZE        = 16
+//     JUMPTABLE_BIT_WIDTH     = 8
+//
+// Explanation of some of the notation:
+//     all functions are titled run_<some binary value>. the binary value specifies where in the jumptable
+//     the function belongs. if the binary value contains variables as bits (let's call them bit-variables), 
+//     then those bits can be either 0 or 1, and the jumptable is updated accordingly. if you begin a function 
+//     with @IF(<insert bit-variable here>), then the line is only included in entries of the jumptable where
+//     that specific bit-variable is 1. if a function is preceded by @EXCLUDE(<some binary value>), then
+//     the function will not be inserted in the jumptable at that binary value. - is used as a wildcard in
+//     @EXCLUDE. @DEFAULT is used for a default function (in this case, the default is a nop function).
+//     default functions are used when no other function matches the jumptable. this all was intended as a way 
+//     to make the code cleaner and more readable.
+
 #include <iostream>
 using namespace std;
 
-//       765432--
-void run_0000PB() {
-    @IF(P) std::cout << P << std::endl;
-    std::cout << B << std::endl;
+@DEFAULT()
+void nop() {
+
+}
+
+// move shifted register
+@EXCLUDE(00011---)
+void run_000OPABC() {
+
+}
+
+// add and subtract
+void run_000111OA() {
+
+}
+
+// move, compare, add, and subtract immediate
+void run_001OPABC() {
+
+}
+
+// ALU operation
+void run_010000PC() {
+
+}
+
+// high register operations and branch exchange
+void run_010001OP() {
+
+}
+
+// pc-relative load
+void run_01001REG() {
+
+}
+
+// load and store with relative offset
+void run_0101LB0R() {
+
+}
+
+// load and store sign-extended byte and halfword
+void run_0101HS1R() {
+
+}
+
+// load and store with immediate offset
+void run_011BLOFS() {
+
+}
+
+// load and store halfword
+void run_1000LOFS() {
+
+}
+
+// sp-relative load and store
+void run_1001LREG() {
+
+}
+
+// load address
+void run_1010SREG() {
+
+}
+
+// add offset to stack pointer
+void run_10110000() {
+
+}
+
+// push and pop registers()
+void run_1011L10R() {
+
+}
+
+// multiple load and store
+void run_1100LREG() {
+
+}
+
+// conditional branch
+@EXCLUDE(11011111)
+void run_1101COND() {
+
+}
+
+// software interrupt
+void run_11011111() {
+
+}
+
+// unconditional branch
+void run_11100OFS() {
+
+}
+
+// long branch with link
+void run_1111HOFS() {
+
 }
