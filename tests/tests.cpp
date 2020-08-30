ac1728a551a25b2a6e25b51ea7113b58d1f84b78
ZaydQumsieh <zayd@qumsieh.net>
2020-08-30 14:23:23 -0700

    Added LDRH #2 instruction
    Former-commit-id: 10aa4fdd27c402e5a397decc138e25787ee5735e

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -921,3 +921,31 @@ TEST_CASE("CPU Thumb Mode - LDRB (Relative Offset)") {
         REQUIRE(memory.regs[4] == 0x00000053);
     }
 }
+
+
+
+
+
+TEST_CASE("CPU Thumb Mode - LDRH (Relative Offset)") {
+    memory.regs[2] = 0x05000000;
+    memory.main[0x05000000] = 0x42;
+    memory.main[0x05000001] = 0x53;
+    memory.main[0x05000002] = 0x99;
+
+
+    SECTION("LDRH R4, [R2, R3] (Zero offset)") {
+        memory.regs[3] = 0x00000000;
+        memory.regs[4] = 0x00000000;
+        execute(0b0101101'010'011'100);
+
+        REQUIRE(memory.regs[4] == 0x00005342);
+    }
+
+    SECTION("LDRH R4, [R2, R3] (Non-Zero offset)") {
+        memory.regs[3] = 0x00000001;
+        memory.regs[4] = 0x00000000;
+        execute(0b0101101'010'011'100);
+
+        REQUIRE(memory.regs[4] == 0x00009953);
+    }
+}
