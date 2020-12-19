51ac243ce2eaa4988605212bdac09ce12bdf0563
ZaydQumsieh <zayd@qumsieh.net>
2020-12-18 16:00:14 -0800

    yacc functional
    Former-commit-id: b7d22241dd2763d6453ad66362559a6c9fb24c26

--- src/cpp-jump/cpp-jump-compiler.py
+++ src/cpp-jump/cpp-jump-compiler.py
@@ -1,4 +1,7 @@
-import ply.lex as lex
+import ply.lex  as lex
+import ply.yacc as yacc
+
+############################################### OVERVIEW ###############################################
 
 # C++ Jump is a superset of C++ that was created to help fill in a jumptable without having
 # unnecessarily reptitive code. This can be used in the creation of emulators. One of the
@@ -17,7 +20,7 @@ import ply.lex as lex
 # more Exclude statements, and one or more Components. The Include statements tell provide
 # information as to where in the jumptable this particular instruction should appear. For example,
 # a Rule with an Include of 010011010010 would appear in the jumptable at index 010011010010.
-# Includes can get more complicated. For example, you can have a "-" in the binary expression,
+# Includes can get more complicated. For example, you can have a '-' in the binary expression,
 # which means that any bit can appear in that slot. So, a Rule with an Include of 01001101001-
 # would appear in both 010011010010 and 010011010011. Rules can also have zero or more Exclude
 # statements, which means that if a Rule matches the Include but also matches the Exclude, then
@@ -29,7 +32,7 @@ import ply.lex as lex
 # then the code would execute properly. Formats can be thought of similarly to Includes, but
 # they serve a wildly different purpose. Formats are constructed as a binary number with both dashes
 # and capital letters inserted into the expression. The capital letters can be used to alter flow
-# of the C++ code block. This can be achieved using "@IF()" statements within the C++ code block.
+# of the C++ code block. This can be achieved using '@IF()' statements within the C++ code block.
 #
 # Here's an example of a Component to explain how this works:
 # [COMPONENT ADD]
@@ -40,8 +43,8 @@ import ply.lex as lex
 #
 #
 # In this example, whenever the ADD Component is used, if the B bit happens to be 1, then only
-# the line "uint32_t result = 2;" is added. If the B bit is 0, then only the line "uint32_t 
-# result = 3;" is added. This can be used to make program flow dependent on the values of certain
+# the line 'uint32_t result = 2;' is added. If the B bit is 0, then only the line 'uint32_t 
+# result = 3;' is added. This can be used to make program flow dependent on the values of certain
 # bits in the instruction.
 #
 
@@ -64,55 +67,232 @@ import ply.lex as lex
 # an overview for the following regex. Because of this, some of the of the regex shown below match things
 # not in the EBNF like left and right square brackets to make grammar checking easier.
 
+
+
+
+
+################################################ LEXER ################################################
+
 # here's some regex to help match the above, as well as some associated tokens
 
-tokens = [
+
+reserved = [
     'RULE',
     'COMPONENT',
-
     'INCLUDE',
     'EXCLUDE',
-    'FORMAT',
+    'FORMAT'
+]
 
+tokens = [
     'IDENTIFIER',
-    'BINARY_SEQUENCE',
-    'FORMATTED_BINARY_SEQUENCE',
+    'BINARY',
+    'BINARY_VARIABLE',
 
     'LBRACKET',
     'RBRACKET',
     'DASH',
     'COLON',
-    'SLASH'
-]
+    'SLASH',
+    'NEWLINE'
+] + reserved
 
-t_ignore                    = r' '
+states = (
+    ('binary', 'exclusive'),
+)
 
-t_RULE                      = r'RULE'
-t_COMPONENT                 = r'COMPONENT'
+t_ignore          = r' '
 
-t_INCLUDE                   = r'INCLUDE'
-t_EXCLUDE                   = r'EXCLUDE'
-t_FORMAT                    = r'FORMAT'
+t_BINARY          = r'[01]'
+t_BINARY_VARIABLE = r'[A-Z]'
 
-t_IDENTIFIER                = r'[A-Za-z_][A-Za-z_0-9]*'
-t_BINARY_SEQUENCE           = r'[01-]+'
-t_FORMATTED_BINARY_SEQUENCE = r'[01A-Z-]'
+t_LBRACKET        = r'\['
+t_RBRACKET        = r'\]'
+t_DASH            = r'-'
+t_COLON           = r':'
+t_SLASH           = r'/'
 
-t_LBRACKET                  = r'\['
-t_RBRACKET                  = r'\]'
-t_DASH                      = r'-'
-t_COLON                     = r':'
-t_SLASH                     = r'/'
+t_binary_ignore   = r' '
+t_binary_COLON    = r':'
+t_binary_BINARY          = r'[01]'
+t_binary_BINARY_VARIABLE = r'[A-Z]'
+t_binary_DASH            = r'-'
 
+def t_IDENTIFIER(t):
+    r'[A-Za-z_][A-Za-z_0-9]*'
+
+    # handle reserved keywords that may have been wrongly interpretted as an identifier
+    if t.value in reserved:
+        t.type = t.value
+    
+    if t.value in ['FORMAT']:
+        t.lexer.begin('binary')
+
+    return t
+
+# https://www.dabeaz.com/ply/ply.html#ply_nn23
 # Define a rule so we can track line numbers
 def t_newline(t):
     r'\n+'
-    t.lexer.lineno += len(t.value)
+    t.lexer.lineno += 1
+    t.type = 'NEWLINE'
+    return t
 
+def t_binary_newline(t):
+    r'\n+'
+    t.lexer.lineno += 1
+    t.lexer.begin('INITIAL')
+    t.type = 'NEWLINE'
+    return t
+
+# On error, we report the illegal character and skip to continue.
+# We also set a flag to let the compiler know that it shouldn't
+# parse the tokens to ensure they're grammatically correct.
+error_found = False
+
+def t_error(t):
+    global error_found
+    error_found = True
+
+    print('Illegal character "%s"' % t.value[0])
+    t.lexer.skip(1)
+
+def t_binary_error(t):
+    global error_found
+    error_found = True
+
+    print('Illegal character "%s"' % t.value[0])
+    t.lexer.skip(1)
+
+# set up the lexer
 lexer = lex.lex()
-with open("test.cpp", 'r') as f:
-    lexer.input(f.read())
-    
-# tokenize
-for tok in lexer:
-    print(tok)
+
+
+
+############################################ SYNTAX ANALYSIS ############################################
+# Here, we generate an AST given the tokens generated beforehand
+
+# Example of a valid complete_rule:
+# [RULE RULE1]
+# - INCLUDE: 10--
+# - EXCLUDE: ---1
+# - COMPONENT: ADDRESSING_MODE_1
+# - COMPONENT: ADD
+# [/RULE]
+
+# And the AST it generates:
+# ['RULE', [['INCLUDE', ['1', '0', '-', '-']], 
+#           ['EXCLUDE', ['-', '-', '-', '1']], 
+#           ['COMPONENT', 'ADDRESSING_MODE_1'], 
+#           ['COMPONENT', 'ADD']]
+
+# Example of a valid complete_component:
+# [COMPONENT ADDRESSING_MODE_1]
+# - FORMAT: 1ABC
+# [/COMPONENT]
+
+# And the AST it generates:
+# ['COMPONENT', ['1', 'A', 'B', 'C']]
+
+# The default rule - the AST will be full of a collection of "complete_items"
+# The following two functions are a list pattern for list_complete_item. One handles the case where
+# the list has one item, one where it has many items. This pattern will appear many times.
+def p_list_complete_item_single(p):
+    '''list_complete_item : complete_item'''
+    p[0] = [p[1]]
+
+def p_list_complete_item_group(p):
+    '''list_complete_item : list_complete_item complete_item'''
+    p[0] = p[1] + [p[2]]
+
+def p_complete_item(p):
+    '''complete_item : complete_rule
+                     | complete_component'''
+    p[0] = p[1]
+
+def p_complete_rule(p):
+    'complete_rule : rule_header list_rule_component rule_footer'
+    p[0] = [p[1], p[2]]
+
+def p_rule_header(p):
+    'rule_header : LBRACKET RULE IDENTIFIER RBRACKET NEWLINE'
+    p[0] = p[2]
+
+def p_list_rule_component_single(p):
+    'list_rule_component : rule_component'
+    p[0] = [p[1]]
+
+def p_list_rule_component_group(p):
+    'list_rule_component : list_rule_component rule_component'
+    p[0] = p[1] + [p[2]]
+
+def p_rule_component(p):
+    '''rule_component : include_statement
+                      | exclude_statement
+                      | component_statement'''
+    p[0] = p[1]
+
+def p_include_statement(p):
+    'include_statement : DASH INCLUDE COLON list_binary_item NEWLINE'
+    p[0] = ["INCLUDE", p[4]]
+
+def p_list_binary_item_single(p):
+    '''list_binary_item : binary_item'''
+    p[0] = [p[1]]
+
+def p_list_binary_item_group(p):
+    '''list_binary_item : list_binary_item binary_item'''
+    p[0] = p[1] + [p[2]]
+
+def p_binary_item(p):
+    '''binary_item : BINARY
+                   | DASH'''
+    p[0] = p[1]
+
+def p_exclude_statement(p):
+    'exclude_statement : DASH EXCLUDE COLON list_binary_item NEWLINE'
+    p[0] = ["EXCLUDE", p[4]]
+
+def p_component_statement(p):
+    'component_statement : DASH COMPONENT COLON IDENTIFIER NEWLINE'
+    p[0] = ["COMPONENT", p[4]]
+
+def p_rule_footer(p):
+    'rule_footer : LBRACKET SLASH RULE RBRACKET NEWLINE'
+
+def p_complete_component(p):
+    'complete_component : component_header format_statement component_footer'
+    p[0] = ["COMPONENT", p[2]]
+
+def p_component_header(p):
+    'component_header : LBRACKET COMPONENT IDENTIFIER RBRACKET NEWLINE'
+
+def p_format_statement(p):
+    'format_statement : DASH FORMAT COLON list_formatted_binary_item NEWLINE'
+    p[0] = p[4]
+
+def p_list_formatted_binary_item_single(p):
+    'list_formatted_binary_item : formatted_binary_item'
+    p[0] = [p[1]]
+
+def p_list_formatted_binary_item_group(p):
+    'list_formatted_binary_item : list_formatted_binary_item formatted_binary_item'
+    p[0] = p[1] + [p[2]]
+
+def p_formatted_binary_item(p):
+    '''formatted_binary_item : binary_item
+                             | BINARY_VARIABLE'''
+    p[0] = p[1]
+
+def p_component_footer(t):
+    'component_footer : LBRACKET SLASH COMPONENT RBRACKET NEWLINE'
+
+parser = yacc.yacc()
+
+ast = None
+with open('test.cpp', 'r') as f:
+    ast = parser.parse(f.read() + '\n')
+
+print(ast)
+
+############################################ Syntax Checking ############################################
