8226186223762d483b4c9b11c8573952d4fb365d
redthing1 <redthing1@localhost>
2021-03-07 15:07:33 -0800

    port gba header
    Former-commit-id: c92e08a425c51b85ba73ac7df75cb0c7e627c033

--- src/emu/source/gba.d
+++ src/emu/source/gba.d
@@ -1,21 +1,18 @@
 module gba;
 
 import core.stdc.stdint; //uint16_t uint32_t 
+import memory;
+import arm7tdmi;
+import ppu;
 
-auto const CART_SIZE = 0x1000000;
+enum CART_SIZE = 0x1000000;
 
-auto const ROM_ENTRY_POINT = 0x000;
-auto const GAME_TITLE_OFFSET = 0x0A0;
-auto const GAME_TITLE_SIZE = 12;
+enum ROM_ENTRY_POINT = 0x000;
+enum GAME_TITLE_OFFSET = 0x0A0;
+enum GAME_TITLE_SIZE = 12;
 
 class GBA {
-    //friend void error(string message
-    );
-    bool enabled;
-private:
-    int[] cpu;
-    int[] ppu;
-    int[] memory;
+public:
     struct DMAChannel {
         uint32_t[] source;
         uint32_t[] dest;
@@ -26,6 +23,26 @@ private:
         uint16_t size_buf;
         bool enabled;
     }
+
+    void error(string message);
+
+    // TODO: run the GBA. probably going to be one of the last things that is actually implemented, since manually cycling
+    // the emulator is a lot easier to test. heck, this method might not even exist, idk. but, it's here for now.
+    void run(string rom_name);
+
+    // cycles the GBA CPU once, executing one instruction to completion.
+    // maybe this method belongs in an ARM7TDMI class. nobody knows. i don't see the reason for having such a class, so
+    // this is staying here for now.
+    void cycle();
+
+    // returns true if a DMA transfer occurred this cycle.
+    bool handle_dma();
+
+    bool enabled;
+private:
+    int[] cpu;
+    int[] ppu;
+    int[] memory;
 
     alias DMAChannel_t = GBA.DMAChannel;
     GBA.DMAChannel_t[4] dma_channels;
