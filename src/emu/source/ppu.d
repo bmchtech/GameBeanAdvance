10720de6734c43078285bbd8add8b77aea5343bf
redthing1 <redthing1@localhost>
2021-03-07 14:52:04 -0800

    add some initial d ports
    Former-commit-id: b6de04035af01a602d5436939db749f3708a8551

---
+++ src/emu/source/ppu.d
@@ -0,0 +1,21 @@
+import emu.memory;
+
+class PPU {
+    // General information:
+    // - Contains 227 scanlines, 160+ is VBLANK. VBLANK is not set on scanline 227.
+    // - HBLANK is constantly toggled
+    // - Although the drawing time is only 960 cycles (240*4), the H-Blank flag is "0" for a total of 1006 cycles.
+
+public:
+    PPU(Memory * memory) void cycle() {
+
+    }
+
+private:
+    Memory* memory;
+    ushort dot; // the horizontal counterpart to scanlines.
+
+    void render_background_mode0(uint16_t bgcnt, uint16_t bghofs, uint16_t bgvofs) {
+
+    }
+}
