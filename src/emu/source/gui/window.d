c95dd244cde53c931d3244d1c87cc5ce103d0a93
redthing1 <redthing1@localhost>
2021-03-07 17:14:15 -0800

    add stub SDL
    Former-commit-id: f839fc707d0bfb7ba924a08265d788d5de8b23f0

---
+++ src/emu/source/gui/window.d
@@ -0,0 +1,18 @@
+module gui.window;
+
+import gba;
+
+class GameBeanWindow {
+public:
+    enum WINDOW_SCALE = 2;
+
+    void run() {
+    }
+
+    void exit() {
+    }
+
+private:
+    bool silent_mode;
+    GBA* gba;
+}
