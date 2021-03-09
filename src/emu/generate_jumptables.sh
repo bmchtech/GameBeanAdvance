2758ab21900a21003c49f905c7ae3975dcb1e783
redthing1 <redthing1@localhost>
2021-03-08 18:59:49 -0800

    use pypy3

--- src/emu/generate_jumptables.sh
+++ src/emu/generate_jumptables.sh
@@ -4,10 +4,10 @@ set -o xtrace
 
 pushd .
 cd ../d-jump/source/
-python3 ./compile.py ../../emu/jumptable/jumptable-arm.jpp ../../emu/jumptable/jumptable_arm.d
+pypy3 ./compile.py ../../emu/jumptable/jumptable-arm.jpp ../../emu/jumptable/jumptable_arm.d
 popd
 
 pushd .
 cd ./jumptable
-python3 make-jumptable.py jumptable-thumb-config.cpp jumptable_thumb.d jumptable_thumb.d 16 8 jumptable_thumb JUMPTABLE_THUMB_H uint16_t instruction_thumb
+pypy3 make-jumptable.py jumptable-thumb-config.cpp jumptable_thumb.d jumptable_thumb.d 16 8 jumptable_thumb JUMPTABLE_THUMB_H uint16_t instruction_thumb
 popd
