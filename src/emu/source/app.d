d6406e2a00b14771b8e304a4ffdbb77d7a6349c7
xdrie <xdrie@alt.icu>
2021-03-09 12:54:56 -0800

    add a basic sdl renderer loop

--- src/emu/source/app.d
+++ src/emu/source/app.d
@@ -1,5 +1,27 @@
 import std.stdio;
+import bindbc.sdl;
+import renderer;
+import gba;
 
 void main() {
-	writeln("Hello, world!");
+	SDLSupport ret = loadSDL();
+	if (ret != sdlSupport) {
+		if (ret == SDLSupport.badLibrary) {
+			stderr.writeln("bad sdl library");
+		} else if (ret == SDLSupport.noLibrary) {
+			stderr.writeln("no sdl library");
+		}
+	}
+	writeln("loaded sdl2");
+
+	// TODO: fix GBA loading
+	auto mem = new Memory();
+	writeln("loaded mem");
+	GBA gba = new GBA(mem);
+	writeln("loaded gba");
+
+	writeln("running sdl2 renderer");
+	auto ren = new GameBeanSDLRenderer(gba);
+	ren.init();
+	ren.run();
 }
