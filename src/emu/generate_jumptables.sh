02256a8222f048a2cadfe4d9e8a9a3e648ca06da
redthing1 <redthing1@localhost>
2021-03-08 19:11:56 -0800

    use makefile for jumptables

--- src/emu/generate_jumptables.sh
+++ src/emu/generate_jumptables.sh
@@ -3,11 +3,6 @@
 set -o xtrace
 
 pushd .
-cd ../d-jump/source/
-pypy3 ./compile.py ../../emu/jumptable/jumptable-arm.jpp ../../emu/jumptable/jumptable_arm.d
-popd
-
-pushd .
 cd ./jumptable
-pypy3 make-jumptable.py jumptable-thumb-config.cpp jumptable_thumb.d jumptable_thumb.d 16 8 jumptable_thumb JUMPTABLE_THUMB_H uint16_t instruction_thumb
+make all
 popd
