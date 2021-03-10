35fb4ea918d09f2a422f2778cc33c9f7872b6ac7
zqumsieh <zayd@qumsieh.net>
2021-03-10 15:23:32 -0800

    fixed weird BX bug

--- src/emu/jumptable/jumptable-thumb-config.d
+++ src/emu/jumptable/jumptable-thumb-config.d
@@ -543,6 +543,9 @@ void run_01000110(ushort opcode) {
 // branch exchange
 void run_01000111(ushort opcode) {
    uint pointer = cpu.regs[get_nth_bits(opcode, 3, 7)];
+   //if (get_nth_bits(opcode, 3, 7) == 15) pointer += 2;
+   import std.stdio;
+   writeln(format("POINTER %x", pointer));
    *cpu.pc = pointer & 0xFFFFFFFE; // the PC must be even, so we & with 0xFFFFFFFE.
    cpu.set_bit_T(pointer & 1);
 
