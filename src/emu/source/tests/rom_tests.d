b8254b5d1e6343fc0e2cb30cc9df3ed588a62d3a
zqumsieh <zayd@qumsieh.net>
2021-03-09 18:24:06 -0800

    fixed regs and fault constructors

--- src/emu/source/tests/rom_tests.d
+++ src/emu/source/tests/rom_tests.d
@@ -55,7 +55,7 @@ void test_thumb_mode(string gba_file, string log_file, int num_instructions) {
     CpuState[] expected_output = produce_expected_cpu_states(log_file, num_instructions);
     
     ubyte[] rom = get_rom_as_bytes(gba_file);
-    cpu.memory.rom_1[0..0x02000000] = rom[0..rom.length];
+    cpu.memory.main[Memory.OFFSET_ROM_1..(Memory.OFFSET_ROM_1 + rom.length)] = rom[0..rom.length];
 
     set_cpu_state(cpu, expected_output[0]);
     cpu.set_mode(cpu.MODE_SYSTEM);
@@ -87,7 +87,7 @@ void test_arm_mode(string gba_file, string log_file, int num_instructions, int s
     CpuState[] expected_output = produce_expected_cpu_states(log_file, num_instructions);
     
     ubyte[] rom = get_rom_as_bytes(gba_file);
-    cpu.memory.rom_1[0..0x01000000] = rom[0..rom.length];
+    cpu.memory.main[Memory.OFFSET_ROM_1..Memory.OFFSET_ROM_1 + Memory.SIZE_ROM_1] = rom[0..rom.length];
 
     set_cpu_state(cpu, expected_output[0]);
     cpu.set_bit_T(true);
