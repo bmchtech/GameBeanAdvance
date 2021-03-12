bbab93df3592200e10423f621f6d5a4735a4664b
zqumsieh <zayd@qumsieh.net>
2021-03-11 23:27:07 -0800

    ppu sprites are weird lmao

--- src/emu/source/gba.d
+++ src/emu/source/gba.d
@@ -38,7 +38,7 @@ public:
         this.ppu     = new PPU(memory);
         this.enabled = false;
 
-        cpu.set_mode(arm7tdmi.ARM7TDMI.MODE_SYSTEM);
+        cpu.set_mode(cpu.MODE_SYSTEM);
     }
 
     struct DMAChannel {
