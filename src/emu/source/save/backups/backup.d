095f70b4b9940543fdd052ac5f32963daa159b13
zqumsieh <zayd@qumsieh.net>
2021-09-28 16:15:28 -0700

    un-templated r/w backup functions

--- src/emu/source/save/backups/backup.d
+++ src/emu/source/save/backups/backup.d
@@ -1,8 +1,12 @@
 module save.backups.backup;
 
 class Backup {
-    T    read(T) (uint address)         { return 0x0;}
-    void write(T)(uint address, T data) { return; }
+    uint   read_word     (uint address)              { return 0x0;}
+    ushort read_halfword (uint address)              { return 0x0;}
+    ubyte  read_byte     (uint address)              { return 0x0;}
+    void   write_word    (uint address, uint data)   { return; }
+    void   write_halfword(uint address, ushort data) { return; }
+    void   write_byte    (uint address, ubyte data)  { return; }
 
     ubyte[] serialize()                 { return [];}
     void    deserialize(ubyte[] data)   { return; }
