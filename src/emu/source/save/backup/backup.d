de939fff49d3a16f3ddb315ba6f7159d746f5d49
zqumsieh <zayd@qumsieh.net>
2021-09-28 13:23:24 -0700

    connected backups to memory

--- src/emu/source/save/backup/backup.d
+++
@@ -1,9 +0,0 @@
-module backup.backup;
-
-abstract class Backup {
-    abstract T    read(T) (uint address);
-    abstract void write(T)(uint address, T data);
-
-    abstract ubyte[] serialize();
-    abstract void    deserialize(ubyte[] data);
-}
