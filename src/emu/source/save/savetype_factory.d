fd4493b1a3ecaf504cce0a0a090146b15aaee131
zqumsieh <zayd@qumsieh.net>
2021-09-28 18:24:47 -0700

    fixed flash manufacturer/device IDs being mismatched

--- src/emu/source/save/savetype_factory.d
+++ src/emu/source/save/savetype_factory.d
@@ -6,16 +6,16 @@ import util;
 
 public Backup create_savetype(Savetype savetype) {
     switch (savetype) {
-        case Savetype.Flash_512k_Atmel_RTC:     return new Flash(65536,     false, 1, 0x62, 0x13);
-        case Savetype.Flash_512k_Atmel:         return new Flash(65536,     false, 1, 0x62, 0x13);
-        case Savetype.Flash_512k_SST_RTC:       return new Flash(65536,     false, 1, 0x62, 0x13);
-        case Savetype.Flash_512k_SST:           return new Flash(65536,     false, 1, 0x62, 0x13);
-        case Savetype.Flash_512k_Panasonic_RTC: return new Flash(65536,     false, 1, 0x62, 0x13);
-        case Savetype.Flash_512k_Panasonic:     return new Flash(65536,     false, 1, 0x62, 0x13);
-        case Savetype.Flash_1M_Macronix_RTC:    return new Flash(65536 * 2, true,  2, 0x32, 0x1B);
-        case Savetype.Flash_1M_Macronix:        return new Flash(65536 * 2, true,  2, 0x32, 0x1B);
-        case Savetype.Flash_1M_Sanyo_RTC:       return new Flash(65536 * 2, true,  2, 0x32, 0x1B);
-        case Savetype.Flash_1M_Sanyo:           return new Flash(65536 * 2, true,  2, 0x32, 0x1B);
+        case Savetype.Flash_512k_Atmel_RTC:     return new Flash(65536,     false, 1, 0x32, 0x1B);
+        case Savetype.Flash_512k_Atmel:         return new Flash(65536,     false, 1, 0x32, 0x1B);
+        case Savetype.Flash_512k_SST_RTC:       return new Flash(65536,     false, 1, 0x32, 0x1B);
+        case Savetype.Flash_512k_SST:           return new Flash(65536,     false, 1, 0x32, 0x1B);
+        case Savetype.Flash_512k_Panasonic_RTC: return new Flash(65536,     false, 1, 0x32, 0x1B);
+        case Savetype.Flash_512k_Panasonic:     return new Flash(65536,     false, 1, 0x32, 0x1B);
+        case Savetype.Flash_1M_Macronix_RTC:    return new Flash(65536 * 2, true,  2, 0x62, 0x13);
+        case Savetype.Flash_1M_Macronix:        return new Flash(65536 * 2, true,  2, 0x62, 0x13);
+        case Savetype.Flash_1M_Sanyo_RTC:       return new Flash(65536 * 2, true,  2, 0x62, 0x13);
+        case Savetype.Flash_1M_Sanyo:           return new Flash(65536 * 2, true,  2, 0x62, 0x13);
 
         default: warning("Invalid savetype given"); return null;
     }
