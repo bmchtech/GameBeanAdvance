33825a61a34806cde0de923bec0b3fd4667f67c2
SmallBeanMachine <zayd@qumsieh.net>
2021-11-06 23:38:53 -0700

    prefetch buffer should fetch whole words in ARM mode

--- src/emu/source/hw/memory/prefetch-buffer.d
+++ src/emu/source/hw/memory/prefetch-buffer.d
@@ -7,12 +7,13 @@ import abstracthw.memory;
 import std.stdio;
 
 class PrefetchBuffer {
-    private uint   current_buffer_size;
-    private Memory memory;
-    private uint   current_address;
-    private uint   cycles_till_access_complete;
-    private bool   currently_prefetching;
-    private bool   enabled;
+    private uint       current_buffer_size;
+    private Memory     memory;
+    private uint       current_address;
+    private uint       cycles_till_access_complete;
+    private bool       currently_prefetching;
+    private bool       enabled;
+    private AccessSize prefetch_access_size;
 
     this(Memory memory) {
         this.memory                      = memory;
@@ -30,8 +31,9 @@ class PrefetchBuffer {
         while (this.current_buffer_size < 8 && num_cycles >= cycles_till_access_complete) {
             num_cycles -= cycles_till_access_complete;
         
-            this.current_buffer_size++;
-            start_new_prefetch(current_address + 1);
+            uint increment = this.prefetch_access_size == AccessSize.HALFWORD ? 1 : 2;
+            this.current_buffer_size += increment;
+            start_new_prefetch(current_address + increment, this.prefetch_access_size);
             writefln("advancing.");
         }
 
@@ -45,19 +47,20 @@ class PrefetchBuffer {
         this.currently_prefetching = false;
     }
     
-    pragma(inline, true) void start_new_prefetch(uint address) {
+    pragma(inline, true) void start_new_prefetch(uint address, AccessSize prefetch_access_size) {
         if (!enabled) return;
 
         this.currently_prefetching = true;
 
-        uint current_region = memory.get_region(address << 1);
-        this.current_address = address;
+        uint current_region       = memory.get_region(address << 1);
+        this.current_address      = address;
+        this.prefetch_access_size = prefetch_access_size;
 
-        this.cycles_till_access_complete = memory.waitstates[current_region][AccessType.SEQUENTIAL][AccessSize.HALFWORD];
+        this.cycles_till_access_complete = memory.waitstates[current_region][AccessType.SEQUENTIAL][prefetch_access_size];
         writefln("%x %x", address << 1, this.cycles_till_access_complete);
     }
 
-    pragma(inline, true) ushort request_data_from_rom(uint address, AccessType access_type) {
+    pragma(inline, true) T request_data_from_rom(T)(uint address, AccessType access_type) {
         uint masked_address = address & 0xFF_FFFF;
 
         if (this.enabled && this.currently_prefetching) {
@@ -69,9 +72,13 @@ class PrefetchBuffer {
                 memory.m_cycles += this.cycles_till_access_complete;
 
                 this.invalidate();
-                this.start_new_prefetch(current_address + 1);
+                if (this.prefetch_access_size == AccessSize.HALFWORD) {
+                    this.start_new_prefetch(current_address + 1, this.prefetch_access_size);
+                } else { //                   == AccessSize.WORD
+                    this.start_new_prefetch(current_address + 2, this.prefetch_access_size);
+                }
 
-                return memory.rom.read(masked_address);
+                return read!T(masked_address);
             }
 
             // is the requested value at the head of the prefetch buffer?
@@ -79,20 +86,31 @@ class PrefetchBuffer {
                 memory.m_cycles++;
 
                 this.current_buffer_size--;
-                return memory.rom.read(masked_address);
+                return read!T(masked_address);
             }
 
             // oh, ok. it's not in the prefetch buffer
             this.invalidate();
-            this.start_new_prefetch(current_address + 1);
+            this.start_new_prefetch(current_address + 1, this.prefetch_access_size);
         }
 
         uint region = ((address << 1) >> 24) & 0xF;
-        memory.m_cycles += memory.waitstates[region][access_type][AccessSize.HALFWORD];
+
+        AccessSize access_size;
+        static if (is(T == ushort)) access_size = AccessSize.HALFWORD;
+        static if (is(T == uint  )) access_size = AccessSize.WORD;
+
+        memory.m_cycles += memory.waitstates[region][access_type][access_size];
         
         this.currently_prefetching = true;
 
-        return memory.rom.read(masked_address);
+        return read!T(masked_address);
+    }
+
+    private T read(T)(uint address) {
+        static if (is(T == ushort)) return memory.rom.read(address);
+        static if (is(T == uint  )) return ((memory.rom.read(address + 1) << 16) | 
+                                             memory.rom.read(address));
     }
 
     void set_enabled(bool enabled) {
