47e33a194fa1c1fff0cffb1b634b63bac3511c44
SmallBeanMachine <zayd@qumsieh.net>
2021-12-01 10:11:30 -0800

    cleaned up print statements

--- src/emu/source/hw/interrupts.d
+++ src/emu/source/hw/interrupts.d
@@ -32,7 +32,7 @@ class InterruptManager {
 
     // interrupt_code must be one-hot
     void interrupt(uint interrupt_code) {
-        writefln("Received interrupt %x %x %x", interrupt_code, interrupt_enable, interrupt_request);
+        // writefln("Received interrupt %x %x %x", interrupt_code, interrupt_enable, interrupt_request);
         // is this specific interrupt enabled
         interrupt_request |= interrupt_code;
         if (interrupt_enable & interrupt_code) {
@@ -91,7 +91,6 @@ public:
     }
 
     ubyte read_IF(int target_byte) {
-        writefln("Reading IF Byte %x %x", target_byte, interrupt_request);
         final switch (target_byte) {
             case 0b0: return (interrupt_request & 0x00FF) >> 0;
             case 0b1: return (interrupt_request & 0xFF00) >> 8;
