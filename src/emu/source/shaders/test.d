92aa1d12d79f87a7457b1deb167517f3f7772e4a
ZaydQumsieh <zayd@qumsieh.net>
2022-01-10 22:24:55 -0800

    added lcd color correction

--- src/emu/source/shaders/test.d
+++ src/emu/source/shaders/test.d
@@ -8,8 +8,7 @@ const GLchar* vertex_shader =
 in vec2 vPosition;
 out vec2 tex_coord;
 
-void main()
-{
+void main() {
     gl_Position = vec4(vPosition, 0.0, 1.0);
     tex_coord   = (vPosition + 1.0) / 2.0;
     tex_coord.y *= -1.0;
@@ -22,9 +21,26 @@ in vec3 fs_color;
 in vec2 tex_coord;
 uniform sampler2D texture2D;
 
+// algorithm developed by Near and Talarubi:
+// https://near.sh/articles/video/color-emulation
+vec4 color_correct(vec4 color) {
+    float lcdGamma = 4.0, outGamma = 2.2;
+    float lb = pow(color.b / 31.0, lcdGamma);
+    float lg = pow(color.g / 31.0, lcdGamma);
+    float lr = pow(color.r / 31.0, lcdGamma);
+
+    vec3 color_without_gamma = vec3(
+        pow((  0.0 * lb +  50.0 * lg + 255.0 * lr) / 255.0, 1.0 / outGamma) * (65535.0 / 280.0),
+        pow(( 30.0 * lb + 230.0 * lg +  10.0 * lr) / 255.0, 1.0 / outGamma) * (65535.0 / 280.0),
+        pow((220.0 * lb +  10.0 * lg +  50.0 * lr) / 255.0, 1.0 / outGamma) * (65535.0 / 280.0)
+    );
+
+    vec3 color_with_gamma = pow(color_without_gamma, vec3(1.0 / outGamma));
+    return vec4(color_with_gamma, 1.0);
+}
+
 out vec4 color_out;
 
-void main()
-{
-    color_out = texture(texture2D, tex_coord);
+void main() {
+    color_out = color_correct(texture(texture2D, tex_coord));
 }";
