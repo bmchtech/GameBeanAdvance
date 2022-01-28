313163215a4791502c24d5c2d5274ffb5265e6b3
ZaydQumsieh <zayd@qumsieh.net>
2022-01-28 02:24:47 -0800

    may need to refactor arm addr mode

--- src/emu/jumptable/source/addressing_mode.d
+++ src/emu/jumptable/source/addressing_mode.d
@@ -6,27 +6,31 @@ import ops;
 
 import std.stdio;
 
-static void create_addressing_mode_2(bool register, bool pre, bool up, bool byte_access, bool writeback, bool load)(IARM7TDMI cpu, Word opcode) {
+static void create_addressing_mode_2(bool is_register_offset, int shift_type, bool pre, bool up, bool byte_access, bool writeback, bool load)(IARM7TDMI cpu, Word opcode) {
     Reg rd = get_nth_bits(opcode, 12, 16);
     Reg rn = get_nth_bits(opcode, 16, 20);
-    
-    static if (register) {
-        // magic!
-        Word address = 0;
+    Word address = cpu.get_reg(rn);
+
+    auto modify_address = (Word amount) {
+        enum dir = up ? 1 : -1;
+        return address + dir * amount;
+    };
+
+    static if (is_register_offset) {
+        static if (shift == 0) {
+            
+        }
     } else {
         auto offset = get_nth_bits(opcode, 0, 12);
         enum dir = up ? 1 : -1;
 
-        static if (pre) {
-            Word address = cpu.get_reg(rn) + dir * offset;
-        } else {
-            Word address = cpu.get_reg(rn);
-        }
+        static if (pre) modify_address(offset);
     }
 
-    writefln("%x %x %x %x", address, rd, load, cpu.get_reg(rd));
     static if ( load &&  byte_access) cpu.ldrb(rd, address);
     static if ( load && !byte_access) cpu.ldr (rd, address);
     static if (!load &&  byte_access) cpu.strb(rd, address);
     static if (!load && !byte_access) cpu.str (rd, address);
+
+    // static if (pre) modify_address(offset);
 }
