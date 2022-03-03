4d231b8e52813dc17478ff430fe601b591fb0e2d
redthing1 <redthing1@alt.icu>
2022-03-02 21:03:22 -0800

    try this

--- source/ui/device/frontend/rengfrontend.d
+++ source/ui/device/frontend/rengfrontend.d
@@ -3,6 +3,7 @@ module ui.device.frontend.rengfrontend;
 import ui.device.frontend.rengcore;
 import ui.device.device;
 import ui.device.event;
+import ui.device.frontend.gbavideo;
 
 class RengFrontend : MultiMediaDevice {
     RengCore reng_core;
@@ -14,8 +15,17 @@ class RengFrontend : MultiMediaDevice {
     override {
         // video stuffs
         void receive_videobuffer(Pixel[SCREEN_HEIGHT][SCREEN_WIDTH] buffer) {
-            
-            reng_core.draw_pub();
+            // reng_core.draw_pub();
+            auto gbavid = Core.jar.resolve!GbaVideo();
+            assert(gbavid, "gba video renderer was null");
+
+            // do stuff here
+            // gbavid.frame_buffer
+            for (int i = 0; i < SCREEN_HEIGHT; i++) {
+                for (int j = 0; j < SCREEN_WIDTH; j++) {
+                    gbavid.frame_buffer[i * SCREEN_WIDTH + j] = buffer[i][j];
+                }
+            }
         }
 
         void reset_fps() {
@@ -27,7 +37,7 @@ class RengFrontend : MultiMediaDevice {
 
         }
 
-        void update() { 
+        void update() {
             reng_core.update_pub();
         }
 
@@ -36,17 +46,30 @@ class RengFrontend : MultiMediaDevice {
             reng_core.draw_pub();
         }
 
+        void pause() {
+        }
 
-        void pause() {}
-        void play() {}
-        uint get_sample_rate() { return 100; }
-        uint get_samples_per_callback() { return 44100; }
-        size_t get_buffer_size() { return 500; }
+        void play() {
+        }
+
+        uint get_sample_rate() {
+            return 100;
+        }
+
+        uint get_samples_per_callback() {
+            return 44100;
+        }
+
+        size_t get_buffer_size() {
+            return 500;
+        }
 
         // input stuffs
-        void handle_input() {}
-        
-        void notify(Event e) {}
+        void handle_input() {
+        }
+
+        void notify(Event e) {
+        }
 
     }
 }
