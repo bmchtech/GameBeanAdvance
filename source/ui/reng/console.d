5beb7fd1e4bd60d01c9f1a7ab5860cce1d711f2d
redthing1 <redthing1@alt.icu>
2024-09-02 16:45:14 -0700

    pass gba instance to console

--- source/ui/reng/console.d
+++ source/ui/reng/console.d
@@ -3,9 +3,16 @@ module ui.reng.console;
 import re.core;
 import re.ng.diag.console;
 
+import hw.gba;
+
 class EmuConsole {
+    GBA gba;
+
     void register() {
         Core.inspector_overlay.console.reset();
+
+        // get the gba instance
+        gba = Core.jar.resolve!GBA().get;
 
         // add emu commands
         Core.inspector_overlay.console.add_command(ConsoleCommand("gamebean", &cmd_gamebean, "gamebean"));
@@ -17,5 +24,6 @@ class EmuConsole {
 
     void cmd_gamebean(string[] args) {
         Core.log.info("GameBeanAdvance Emulator Console");
+        Core.log.info("GBA: %s", gba);
     }
 }
